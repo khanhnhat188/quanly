eff92f567d2153747acbcab0996907fb6d3a8311
khanhnhat188 <teamka71@gmail.com>
2023-12-15 10:41:28 +0700

    csdl

--- index.php
+++ index.php
@@ -12,6 +12,7 @@ include "model/department.php";
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <tile>Quản lý nhân sự</tile>
     <!-- App favicon -->
     <link rel="shortcut icon" href="assets/images/favicon.ico">
 
@@ -47,9 +48,6 @@ include "model/department.php";
 </head>
 
 <body>
-    <div class="wrapper">
-        <div class="navbar-custom">
-        </div>
 
         <?php
     include "sidebar.php";
